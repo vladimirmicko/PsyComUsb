ae38618edceaee91620d77611b36bbe140460210
vladimir <vladimir.randjelovic@kireygroup.com>
2019-04-04 17:27:47 +0200

    ClaimInterface method added

--- src/main/java/com/vladimir/PsyComUsb/App.java
+++ src/main/java/com/vladimir/PsyComUsb/App.java
@@ -2,6 +2,7 @@ package com.vladimir.PsyComUsb;
 
 import java.util.List;
 
+import javax.usb.UsbConfiguration;
 import javax.usb.UsbConst;
 import javax.usb.UsbDevice;
 import javax.usb.UsbDeviceDescriptor;
@@ -10,15 +11,18 @@ import javax.usb.UsbException;
 import javax.usb.UsbHostManager;
 import javax.usb.UsbHub;
 import javax.usb.UsbInterface;
+import javax.usb.UsbInterfacePolicy;
 import javax.usb.UsbServices;
 
 public class App {
 	
-	/** The vendor ID of the led stripe. */
-	private static final short VENDOR_ID = 0x2786;
+	/** The vendor ID of the PsyCom */
+	//private static final short VENDOR_ID = 0x2786;
+	private static final short VENDOR_ID = 0x5986;
 
-	/** The product ID of the led stripe. */
-	private static final short PRODUCT_ID = 0x7750;
+	/** The product ID of the PsyCom */
+	//private static final short PRODUCT_ID = 0x7750;
+	private static final short PRODUCT_ID = 0x2113;
 
 	public static void main(String[] args) {
 		App app = new App();
@@ -32,21 +36,6 @@ public class App {
 
 	}
 	
-	
-	public UsbDevice findDevice(UsbHub hub, short vendorId, short productId)
-	{
-	    for (UsbDevice device : (List<UsbDevice>) hub.getAttachedUsbDevices())
-	    {
-	        UsbDeviceDescriptor desc = device.getUsbDeviceDescriptor();
-	        if (desc.idVendor() == vendorId && desc.idProduct() == productId) return device;
-	        if (device.isUsbHub())
-	        {
-	            device = findDevice((UsbHub) device, vendorId, productId);
-	            if (device != null) return device;
-	        }
-	    }
-	    return null;
-	}
 	
 	protected void initialize() throws SecurityException, UsbException {
 		UsbServices services = UsbHostManager.getUsbServices();
@@ -68,6 +57,35 @@ public class App {
 				}
 			}
 		}
+	}
+	
+	public UsbDevice findDevice(UsbHub hub, short vendorId, short productId)
+	{
+	    for (UsbDevice device : (List<UsbDevice>) hub.getAttachedUsbDevices())
+	    {
+	        UsbDeviceDescriptor desc = device.getUsbDeviceDescriptor();
+	        if (desc.idVendor() == vendorId && desc.idProduct() == productId) return device;
+	        if (device.isUsbHub())
+	        {
+	            device = findDevice((UsbHub) device, vendorId, productId);
+	            if (device != null) return device;
+	        }
+	    }
+	    return null;
+	}
+	
+	
+	public void claimInterface(UsbDevice device) throws UsbException {
+
+		UsbConfiguration configuration = device.getUsbConfiguration((byte) 1);
+		UsbInterface iface = configuration.getUsbInterface((byte) 0);
+		iface.claim(new UsbInterfacePolicy() {
+			public boolean forceClaim(UsbInterface usbInterface) {
+				return true;
+			}
+		});
+
+		
 	}
 	
 	
