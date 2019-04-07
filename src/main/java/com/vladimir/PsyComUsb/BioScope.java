6c7bc5bdf7a62776122d175bad654cad77c85691
Vladimir Randjelovic <72micko@gmail.com>
2019-04-07 19:12:46 +0200

    Reading/Writing USB works

--- src/main/java/com/vladimir/PsyComUsb/BioScope.java
+++ src/main/java/com/vladimir/PsyComUsb/BioScope.java
@@ -15,9 +15,13 @@ import javax.usb.UsbHostManager;
 import javax.usb.UsbHub;
 import javax.usb.UsbInterface;
 import javax.usb.UsbInterfacePolicy;
+import javax.usb.UsbIrp;
 import javax.usb.UsbNotActiveException;
+import javax.usb.UsbNotClaimedException;
+import javax.usb.UsbNotOpenException;
 import javax.usb.UsbPipe;
 import javax.usb.UsbServices;
+import javax.usb.util.DefaultUsbIrp;
 
 public class BioScope {
 
@@ -27,6 +31,7 @@ public class BioScope {
 	private static final short PRODUCT_ID = 0x2720;
 
 	private UsbDevice device;
+	private UsbInterface iface;
 
 	public static void main(String[] args) {
 		BioScope app = new BioScope();
@@ -57,15 +62,33 @@ public class BioScope {
 		}
 
 		System.out.println("USB device found!");
-
+		
+		
 		UsbConfiguration configuration = device.getUsbConfiguration((byte) 1);
-		UsbInterface iface = configuration.getUsbInterface((byte) 0);
+		iface = configuration.getUsbInterface((byte) 0);
+
+		
 		try {
 			iface.claim(new UsbInterfacePolicy() {
 				public boolean forceClaim(UsbInterface usbInterface) {
 					return true;
 				}
 			});
+			
+			byte[] bytes = new byte[64];
+			readCurrentConfiguration();
+
+			bytes = new byte[64];
+			bytes[0] = (byte) 0x73;
+			bytes[1] = (byte) 0x00;
+			app.sendMessage(bytes);
+			
+			bytes = new byte[64];
+			bytes[0] = (byte) 0x88;
+			bytes[1] = (byte) 0x00;
+			app.sendMessage(bytes);
+			app.readUSB();
+			
 		} catch (UsbClaimException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -79,50 +102,45 @@ public class BioScope {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		final byte[] bytes = new byte[64];
-		bytes[0] = (byte) 0x73;
-		bytes[1] = (byte) 0x00;
-
-
-//		try {
-//			app.sendMessage(device, bytes);
-//		} catch (UsbException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-		app.sendMessage(iface, bytes, 0x01);
-//		app.sendMessage(iface, bytes, 0x81);		
-//	    app.readMessage(iface, 0x01);
+		finally {
+			try {
+				iface.release();
+			} catch (UsbClaimException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UsbNotActiveException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UsbDisconnectedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UsbException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
-	public static void sendMessage(UsbDevice device, byte[] message) throws UsbException {
-		UsbControlIrp irp = device.createUsbControlIrp(
-				(byte) (UsbConst.REQUESTTYPE_TYPE_CLASS | UsbConst.REQUESTTYPE_RECIPIENT_INTERFACE), (byte) 0x09,
-				(short) 2, (short) 1);
-		irp.setData(message);
-		device.syncSubmit(irp);
-	}
 
-	public void sendMessage(UsbInterface iface, byte[] bytes, int i) {
+	public void sendMessage(byte[] bytes) {
 		UsbPipe pipe = null;
 
 		try {
-			UsbEndpoint endpoint = (UsbEndpoint) iface.getUsbEndpoint((byte) i);
+			UsbEndpoint endpoint = (UsbEndpoint) iface.getUsbEndpoint((byte) 0x01);
 			pipe = endpoint.getUsbPipe();
 			pipe.open();
 
 			int sent = pipe.syncSubmit(bytes);
 
 			System.out.println(sent + " bytes sent");
-			pipe.close();
+//			pipe.close();
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		} finally {
 			try {
-				iface.release();
+				pipe.close();
+//				iface.release();
 			} catch (UsbClaimException e) {
 				e.printStackTrace();
 			} catch (UsbNotActiveException e) {
@@ -133,6 +151,67 @@ public class BioScope {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	public void readUSB() {
+
+		UsbEndpoint endpoint = iface.getUsbEndpoint((byte) 0x81);
+		UsbPipe pipe = endpoint.getUsbPipe();
+		
+		int maxPacketSize = endpoint.getUsbEndpointDescriptor().wMaxPacketSize( );
+		byte[] buffer = new byte[64];
+		UsbIrp irp1 = pipe.createUsbIrp( );
+		irp1.setData(buffer);
+		irp1.setOffset(0);
+		irp1.setLength(maxPacketSize);
+
+
+		try {
+			pipe.open();
+			int received = pipe.syncSubmit(buffer);
+			System.out.println(received + " bytes received");
+			System.out.println("buffer[0]" + buffer[0]);
+			System.out.println("buffer[1]" + buffer[1]);
+			System.out.println("buffer[2]" + buffer[2]);
+			System.out.println("buffer[3]" + buffer[3]);
+			System.out.println("buffer[4]" + buffer[4]);
+			System.out.println("buffer[5]" + buffer[5]);
+			System.out.println("buffer[6]" + buffer[6]);
+			System.out.println("buffer[7]" + buffer[7]);
+			System.out.println("buffer[8]" + buffer[8]);
+			System.out.println("buffer[9]" + buffer[9]);
+			
+			
+		} catch (UsbNotActiveException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UsbNotClaimedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UsbDisconnectedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UsbException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				pipe.close();
+			} catch (UsbNotActiveException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UsbNotOpenException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UsbDisconnectedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UsbException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 	public UsbDevice findDevice(UsbHub hub, short vendorId, short productId) {
@@ -149,4 +228,24 @@ public class BioScope {
 		return null;
 	}
 
+	public void readCurrentConfiguration() {
+		UsbControlIrp irp = device.createUsbControlIrp(
+				(byte) (UsbConst.REQUESTTYPE_DIRECTION_IN | UsbConst.REQUESTTYPE_TYPE_STANDARD
+						| UsbConst.REQUESTTYPE_RECIPIENT_DEVICE),
+				UsbConst.REQUEST_GET_CONFIGURATION, (short) 0, (short) 0);
+		irp.setData(new byte[1]);
+		try {
+			device.syncSubmit(irp);
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UsbDisconnectedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UsbException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(irp.getData()[0]);
+	}
 }
